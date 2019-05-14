259416abc14797a291d370549800b855fab336c1
dachosen1 <anderson.nelson1@gmail.com>
2019-05-14 16:43:29 -0400

    Fixed warnings

--- page_spyder.py
+++ page_spyder.py
@@ -1,5 +1,5 @@
-import os
 import argparse
+
 
 def main(database: str, url_list_file: str):
     print("we are going to work with" + database)
@@ -13,4 +13,4 @@ if __name__ == '__main__':
     args = parser.parse_args()
     database_file = args.database
     input_file = args.input
-    main(database = database_file, url_list_file= input_file)
+    main(database=database_file, url_list_file=input_file)
